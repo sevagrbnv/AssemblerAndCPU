93deaa5dd23a4fba59dc26ce13bd3abb4550e099
seva_ <sevagrbnv@gmail.com>
2023-10-25 00:54:08 +0300

    little refactoring

--- src/main/kotlin/cpu_components/Memory.kt
+++ src/main/kotlin/cpu_components/Memory.kt
@@ -1,26 +1,26 @@
 package cpu_components
 
-class Memory(override val size: Int = SIZE) : ArrayList<String>(size) {
+class Memory(override val size: Int = MEMORY_SIZE) : ArrayList<String>(size) {
     init {
         reset()
     }
 
     private fun reset() {
-        repeat(SIZE) {
+        repeat(MEMORY_SIZE) {
             this.add("0x0000")
         }
     }
 
     override fun toString(): String {
         val result = StringBuilder()
-        this.chunked(CHUNK).forEach {
+        this.chunked(CHUNK_SIZE).forEach {
             result.appendLine(it.joinToString(" "))
         }
         return "Memory:\n${result}"
     }
 
     companion object {
-        private const val SIZE = 64
-        private const val CHUNK = 8
+        private const val MEMORY_SIZE = 64
+        private const val CHUNK_SIZE = 8
     }
 }
